6ea0871d2dfbe9abca1c758e952c864e8b060b0b
Mohammad Reza Kiani <mohammad.76kiani@gmail.com>
2017-04-03 10:49:28 +0430

    python greeting has been added

---
+++ greating.py
@@ -0,0 +1,6 @@
+name = input("Enter your name(in double qutations): ")
+print ("Hello {}! :)".format(name))
+
+age = int(input("How old are you? "))
+print ("{} kiss for you ;) => {}".format(age, ":*"*age))
+
